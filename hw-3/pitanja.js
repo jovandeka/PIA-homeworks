27f1eff89e76a9c9a172b18ecbf47dc82ab2199c
jovandeka <51538656+jovandeka@users.noreply.github.com>
2020-12-24 17:08:27 +0100

    Update and rename kviz.json to pitanja.js

---
+++ hw-3/pitanja.js
@@ -0,0 +1,91 @@
+var jsonData = [
+    {
+        "q": "Koja od navedenih pesama je najveći hit Azre?",
+        "opt1" : "Fikreta",
+        "opt2" : "Kotor",
+        "opt3" : "Balkan",
+        "opt4" : "Priča o Vasi Ladačkom",
+        "answer": "Balkan"
+    },
+
+    { 
+        "q": "Koji je nadimak gitariste grupe Smak, Radomira Mihajlovića?",
+        "opt1" : "Ubica",
+        "opt2" : "Majstor",
+        "opt3" : "Rade",
+        "opt4" : "Točak",
+        "answer": "Točak"
+    },
+
+    {
+        "q": "Koja od navedenih pesama NIJE pesma Partibrejkers-a?",
+        "opt1" : "Neko te ima noćas",
+        "opt2" : "Mesečeva kći",
+        "opt3" : "Rođen loš",
+        "opt4" : "Kreni prema meni",
+        "answer": "Neko te ima noćas"
+    },
+
+    {
+        "q": "Po kojoj državi nosi naziv hita benda Džukele?",
+        "opt1" : "Srbija",
+        "opt2" : "Belgija",
+        "opt3" : "Amerika",
+        "opt4" : "Zimbabve",
+        "answer": "Amerika"
+    },
+
+    {
+        "q": "Koja je skraćenica benda EKV?",
+        "opt1" : "Epidemija Korona Virusa",
+        "opt2" : "Ekatarina Velika",
+        "opt3" : "Enciklopedija Koja Viče",
+        "opt4" : "Egzil Kralja Vukašina",
+        "answer": "Ekatarina Velika"
+    },
+
+    {
+        "q": "Kako se zove frontmen benda Majke?",
+        "opt1" : "Goran Bare",
+        "opt2" : "Vlada Divljan",
+        "opt3" : "Momćilo Bajagić",
+        "opt4" : "Milos Obilić",
+        "answer": "Goran Bare"
+    },
+
+    {
+        "q": "Ko od navedenih NIJE bio član benda Šarlo Akrobata?",
+        "opt1" : "Dušan Kojić Koja",
+        "opt2" : "Zoran Kostić Cane",
+        "opt3" : "Milan Mladenović",
+        "opt4" : "Ivica Vdović Vd",
+        "answer": "Zoran Kostić Cane"
+    },
+
+    {
+        "q": "Koliko bendova se nalazi na albumu Paket aranžman?",
+        "opt1" : "1",
+        "opt2" : "2",
+        "opt3" : "3",
+        "opt4" : "4",
+         "answer": "3"
+    },
+
+    {
+        "q": "Zabranjeno Pusenje i Riblja Čorba imaju različitu pesmu sa istim nazivom, koja je to pesma?",
+        "opt1" : "Bos ili hadžija",
+        "opt2" : "Dva dinara druže",
+        "opt3" : "Devojčice kojima miriše koža",
+        "opt4" : "Lutka sa naslovne strane",
+        "answer": "Lutka sa naslovne strane"
+    },
+
+    {
+        "q": "Iz kog grada je bend KBO! ?",
+        "opt1" : "Kragujevac",
+        "opt2" : "Novi Sad",
+        "opt3" : "Mladenovac",
+        "opt4" : "Pančevo",
+        "answer": "Kragujevac"
+    }
+];
